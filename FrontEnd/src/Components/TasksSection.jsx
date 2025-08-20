9804337e79958d93d35c3a347b1529aacb55c94a
Navil Talukdar <naviltalukdar3@gmail.com>
2025-08-20 03:11:17 -0400

    minor tweaks

--- FrontEnd/src/Components/TasksSection.jsx
+++ FrontEnd/src/Components/TasksSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function TasksSection() {
   return (
-    <div className='bg-neutral-700 m-5 p-5'>
+    <div className='bg-neutral-700 m-10 p-5 rounded-xl'>
         <div className='lg:flex justify-between'>
             <div className='pl-3'>
                 <h1 className='text-8xl'>Today</h1>
@@ -10,9 +10,11 @@ function TasksSection() {
             </div>
             <div className='lg:text-6xl text-3xl lg:pt-0 pt-5 flex'>
                 <input type='text' placeholder='Add a Task' className='border-amber-500 rounded-l-xl border py-2 focus:outline-none focus:outline-neutral-100 focus:ring-1 pl-5'></input>
-                <button className='border rounded-r-lg border-orange-200 bg-amber-500   hover:bg-amber-500 cursor-pointer'>➕</button>
+                <button className='border rounded-r-lg border-orange-200 bg-amber-500  hover:bg-orange-500 hover:border-orange-500 cursor-pointer'>➕</button>
             </div>
         </div>
+        <ul >
+        </ul>
     </div>
   )
 }
